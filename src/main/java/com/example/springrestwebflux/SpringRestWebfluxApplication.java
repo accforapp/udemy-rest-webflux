ed44af51cc7f5062229b61f8afc492311924dd3c
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2022-02-14 22:31:11 +0200

    Initial commit

---
+++ src/main/java/com/example/springrestwebflux/SpringRestWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringRestWebfluxApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/springrestwebflux/SpringRestWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestWebfluxApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
